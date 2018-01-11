e7fc9eaa9b61acd5dea6ce9538f4c85d76ef84ff
Valerie Wood <valeriewd1@gmail.com>
2018-01-11 03:26:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,19 +36,11 @@ console.log(roughScale('321', 2));
   dec(n);
   return n--;
  
-  7) makeInt(n) parses n as an integer and returns the parsed integer:
-     ReferenceError: makeInt is not defined
-      at Context.<anonymous> (test/index-test.js:34:5)
+  makeInt(n); 
+  parseInt('n');
 
-  8) makeInt(n) assumes base 10:
-     ReferenceError: makeInt is not defined
-      at Context.<anonymous> (test/index-test.js:38:5)
-
-  9) makeInt(n) returns NaN as appropriate:
-     ReferenceError: makeInt is not defined
-      at Context.<anonymous> (test/index-test.js:42:5)
-
-  10) preserveDecimal(n) preserves n's decimals (it parsesn as a floating point number) and returns the parsed number:
+preserveDecimal(n) preserves n's decimals (it parsesn as a floating point number) and 
+returns the parsed number:
      ReferenceError: preserveDecimal is not defined
       at Context.<anonymous> (test/index-test.js:48:5)
 
